4c5e9766abb6eae143f77178ae0a360a4ce865fd
Rail321 <71922301+Rail321@users.noreply.github.com>
2020-10-13 19:03:54 +0300

    Added .gitignore
    Added gulpfile.js
    
    Added gulpfile.js
    
    Added gulp to kit
    
    Added browser-sync to kit
    
    Added gulp-nunjucks to kit
    
    Added gulp-htmlmin to kit
    
    Added filesystem for sources

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const { src, dest, parallel, series, watch}	= require( 'gulp');
+
+const browserSync	= require( 'browser-sync').create();
+const nunjucks		= require( 'gulp-nunjucks');
+const htmlmin		= require( 'gulp-htmlmin');
+
+function browserSyncFunction() {
+	browserSync.init( {
+		server: { baseDir: 'dist/'},
+		notify: false,
+		online: true,
+	})
+}
+
+function htmlProcessingFunction() {
+	return src( 'src/**.njk')
+		.pipe( nunjucks.compile())
+		.pipe( htmlmin( {
+			collapseWhitespace: true,
+		}))
+		.pipe( dest( 'dist/'))
+}
+
+exports.browserSyncFunction		= browserSyncFunction;
+exports.htmlProcessingFunction	= htmlProcessingFunction;
+
+exports.default	= parallel( htmlProcessingFunction, browserSyncFunction);
