03bbd16bc8917fdb0fc6606d628ee1f3178da54e
Rail321 <71922301+Rail321@users.noreply.github.com>
2020-12-01 17:54:57 +0300

    Added sass, autoprefixer and css minifier

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 const { src, dest, parallel, series, watch}	= require( 'gulp');
 
-const browserSync	= require( 'browser-sync').create();
-const nunjucks		= require( 'gulp-nunjucks');
-const htmlmin		= require( 'gulp-htmlmin');
-const beautify		= require( 'gulp-beautify');
-const del			= require( 'del');
+const browserSync		= require( 'browser-sync').create();
+const nunjucks			= require( 'gulp-nunjucks');
+const htmlmin			= require( 'gulp-htmlmin');
+const beautify			= require( 'gulp-beautify');
+const sass				= require( 'gulp-sass');
+const autoprefixer	= require('gulp-autoprefixer');
+const cleanCss			= require( 'gulp-clean-css');
 
 paths = {
 	dist: 'dist/',
-	njk: 'src/njk/*.njk',
+	src: 'src/',
+	njk: 'njk/*.njk',
+	sass: 'sass/main.sass',
+	html: '',
+	css: 'css/',
+	watch: {
+		njk: 'src/njk/**/*.njk',
+		sass: 'sass/**/*.sass',
+	},
 }
 
 function serverHost() {
@@ -20,34 +30,46 @@ function serverHost() {
 	})
 }
 
+function filesWatching() {
+	watch( paths.src + paths.watch.njk, htmlProcessing);
+	watch( paths.src + paths.watch.sass, cssProcessing);
+}
+
 function htmlProcessing() {
-	return src( paths.njk)
+	return src( paths.src + paths.njk)
 		.pipe( nunjucks.compile())
 		.pipe( htmlmin( {
 			collapseWhitespace: true,
 		}))
-		.pipe( dest( paths.dist))
+		.pipe( dest( paths.dist + paths.html))
 		.pipe( browserSync.stream())
 }
 
-function filesWatching() {
-	watch( 'src/**/*.njk', htmlProcessing);
+function cssProcessing() {
+	return src( paths.src + paths.sass)
+		.pipe( sass())
+		.pipe( autoprefixer( {
+			overrideBrowserslist: [ 'last 10 versions'],
+		}))
+		.pipe( cleanCss( {
+			level: {
+				1: {
+					specialComments: 0,
+				}
+			},
+			format: 'uglify',
+		}))
+		.pipe( dest( paths.dist + paths.css))
+		.pipe( browserSync.stream())
 }
 
 function beautifyFiles() {
-	return src( 'src/**.njk')
+	return src( paths.src + paths.njk)
 		.pipe( nunjucks.compile())
 		.pipe( beautify.html( { indent_with_tabs: true}))
-		.pipe( dest( 'dist/'))
+		.pipe( dest( paths.dist + paths.html))
 }
 
-function deleteDist() {
-	del( paths.dist);
-}
-
-exports.serverHost		= serverHost;
-exports.htmlProcessing	= htmlProcessing;
 exports.beautifyFiles	= beautifyFiles;
-exports.deleteDist		= deleteDist;
 
-exports.default	= parallel( deleteDist, htmlProcessing, serverHost, filesWatching);
+exports.default	= parallel( htmlProcessing, cssProcessing, serverHost, filesWatching);
